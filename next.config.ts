e53c297255059808b5f114931429157e44f2c8b6
prapancadv <prapancadev@gmail.com>
2024-10-29 17:20:34 +0000

    Pending changes exported from your codespace

---
+++ next.config.ts
@@ -0,0 +1,13 @@
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+/*  experimental: {
+    serverActions: {
+      allowedOrigins: [
+        'cold-graveyard-97jxv77w6xgqf7x4q-443.app.github.dev','*.cold-graveyard-97jxv77w6xgqf7x4q-443.app.github.dev','localhost:3000'
+      ]
+    }
+  }*/  
+};
+
+export default nextConfig;
